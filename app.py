342ea354ee2dbfd48fc212f1cf27f6dd95602928
Bouchra Mohamed Lemine <bouchramohamedcheikh@gmail.com>
2025-04-06 23:36:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def view_basket():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 5050))  # Default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
